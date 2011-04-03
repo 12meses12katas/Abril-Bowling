5a3ad59a8ed7d75364eb3a597cf81a3f1b180224
StarChox <starchox@gmail.com>
2011-04-04 00:10:35 +0800

    Corrección en el sistema de puntuación, en el calculo de strikes y ultimo frame

--- dobeslao/bowling_func.pl
+++ dobeslao/bowling_func.pl
@@ -25,7 +25,7 @@ sub getSpareBonus {
 
 sub getStrikeBonus {
 	my @nextroll = @_;
-	return getFrameScore(@nextroll);
+	return $nextroll[0];
 }
 
 sub bowling {
@@ -35,16 +35,17 @@ sub bowling {
 	my $VERBOSE = 0;
 
 	for my $frame (0..$#line) {
+
 		my @roll = @{$line[$frame]};
 		my @nextroll = $frame == $#line ? @roll [$#roll-1..$#roll] : @{$line[$frame+1]};
 
 		if (isStrike(@roll)) {
 			if ($VERBOSE) { print "Strike\n"; }
 
-			if ($frame == $#line) { $nextroll[1] = 0; }
 			$score += 10; 
 			$score += getStrikeBonus(@nextroll);
-			$score += $frame < $#line-1 ? getStrikeBonus(@{$line[$frame+2]}) : 0;
+			$score += isStrike(@nextroll) && $frame < $#line-1 ? getStrikeBonus(@{$line[$frame+2]}) 
+									   : getStrikeBonus(@nextroll [1..1]);
 		}
 		elsif (isSpare(@roll)) {
 			if ($VERBOSE) { print "Spare\n"; }
@@ -59,6 +60,7 @@ sub bowling {
 		}
 
 		if ($VERBOSE) {
+			print "Game: " . ($frame+1) . " - ";
 			print "Score: " . $score . "\n";
 			foreach my $roll (0..$#{$line[$frame]}) {
 				print "Throw [$frame][$roll] = $line[$frame][$roll]\n";
