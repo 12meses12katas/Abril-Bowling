037504ad57e7bc60cbf3a7122ed4c315a3eabd94
Pablo Rausch <pablorausch@gmail.com>
2011-04-07 11:08:11 -0300

    C# Two Solutions

--- eltoby/Bowling/Bowling.Test/Program.cs
+++ eltoby/Bowling/Bowling.Test/Program.cs
@@ -8,20 +8,25 @@ namespace Bowling.Test
     {
         static void Main(string[] args)
         {
-            TestRoll[] testRolls = {new TestRoll("XXXXXXXXXXXX",300)
-                                    ,new TestRoll("9-9-9-9-9-9-9-9-9-9-",90)
-                                    , new TestRoll("5/5/5/5/5/5/5/5/5/5/5",150)};
-            
+            TestRoll[] testRolls = {
+                new TestRoll("XXXXXXXXXXXX",300)
+                ,new TestRoll("9-9-9-9-9-9-9-9-9-9-",90)
+                , new TestRoll("5/5/5/5/5/5/5/5/5/5/5",150)
+            };
 
-            
+
+            BowlingCalculator calc = new BowlingCalculator();
             foreach (TestRoll testRoll in testRolls)
             {
                 Line line = new Line(testRoll.Rolls);
                 Console.WriteLine(string.Format("Test: {0}",testRoll.Rolls ));
                 Console.WriteLine(string.Format("Expected result: {0}", testRoll.Score));
-                int calcScore = line.GetScore(testRoll.Rolls);
-                Console.WriteLine(string.Format("Result: {0}", calcScore));
-                Console.WriteLine(string.Format("Test Passed: {0}", testRoll.Test(calcScore)?"Yes":"No"));
+                int calcScore1 = line.GetScore(testRoll.Rolls);
+                int calcScore2 = calc.GetScore(testRoll.Rolls);
+                Console.WriteLine(string.Format("Result Method 1: {0}", calcScore1));
+                Console.WriteLine(string.Format("Test 1 Passed: {0}", testRoll.Test(calcScore1) ? "Yes" : "No"));
+                Console.WriteLine(string.Format("Result Method 2: {0}", calcScore2));
+                Console.WriteLine(string.Format("Test 2 Passed: {0}", testRoll.Test(calcScore2)?"Yes":"No"));
                 Console.WriteLine("-------------");
             }
 
