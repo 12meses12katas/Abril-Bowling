7a62ae3d939d7730ba49839aea738f731bda9a91
Jose Raya <jose.raya@agilogy.com>
2011-04-09 03:39:19 +0800

    Refactoring

--- joseraya/Bowling.php
+++ joseraya/Bowling.php
@@ -1,21 +1,61 @@
 <?php
 
 class Bowling {
-   
-    private $score = 0;
+
+    private $frames = array();
+
     public function __construct($game) {
-        $array = preg_split('//', $game, -1, PREG_SPLIT_NO_EMPTY);
-        foreach($array as $char) {
-            if ($char==="-") {
-                continue;
-            }
-            $this->score += (int)$char;
-        }
-    }
-    
-    public function score() {
-        return $this->score;
+        $this->frames = $this->getFrames($game);
     }
 
- 
+    public function score() {
+        $score = 0;
+        $current = 0;
+        while($current < count($this->frames)) {
+            $frame = $this->frames[$current++];
+            $score += $this->scoreForFrame($frame);
+        }
+        return $score;
+    }
+
+    private function scoreForFrame($frame) {
+        $score = 0;
+        $array = preg_split('//', $frame, -1, PREG_SPLIT_NO_EMPTY);
+        foreach($array as $roll) {
+            if ($this->isMiss($roll)) {
+                continue;
+            }
+            if ($this->isNumber($roll)) {
+                $score += (int) $roll;
+            }
+        }
+        return $score;
+    }
+    
+    private function isMiss($char) {
+        return $char === "-";
+    }
+
+    private function isNumber($char) {
+        return $char > '0' && $char <= '9';
+    }
+
+    private function getFrames($game) {
+        $result = array();
+        $array = preg_split('//', $game, -1, PREG_SPLIT_NO_EMPTY);
+        $current = 0;
+        while ($current < strlen($game)) {
+            $char = $game[$current++];
+            if ($char == 'X') {
+                $frame = 'X';
+            } else {
+                $char2="";//(string)$game{current++};
+                $frame = "$char$char2";
+            }
+            array_push($result, $frame);
+        }
+    
+        return $result;
+    }
+
 }
