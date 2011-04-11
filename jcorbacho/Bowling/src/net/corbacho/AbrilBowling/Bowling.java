9d7578af455506f5c17757ab3745f6b15bffaf26
Jose Corbacho Gil <jose@corbacho.net>
2011-04-11 20:23:07 +0200

    Java version with JUnit tests

--- jcorbacho/Bowling/src/net/corbacho/AbrilBowling/Bowling.java
+++ jcorbacho/Bowling/src/net/corbacho/AbrilBowling/Bowling.java
@@ -2,51 +2,97 @@ package net.corbacho.AbrilBowling;
 
 public class Bowling {
 
-	public static int GetCharValue(char myChar){
+    private int currentFrame;
+    
+    public Bowling() {
+        super();
+        this.currentFrame=0;
+    }
 
-		Character myCharacter = myChar;
-		int value = 0;
-		if (myCharacter.equals('X') || myCharacter.equals('/')) {
-			value = 10;
-		} else if (myCharacter.equals('-')) {
-			value = 0;
-		} else {
-			value = Integer.parseInt(myCharacter.toString());
-		}
-		return value;
-	}
+    public int getTryValue(char myChar) {
 
+        Character myCharacter = myChar;
+        int value = 0;
+        if (myCharacter.equals('X') || myCharacter.equals('/')) {
+            value = 10;
 
-	public static int GetValue(String myString) {
+        } else if (myCharacter.equals('-')) {
+            value = 0;
 
-		int score = 0;
-		for (int i = 0; i < myString.length(); i++) {
-			//TODO Modificar esta condicion
-			if ((myString.length() > 10) && (i < myString.length()-2)) {
-				Character myCharacter = myString.charAt(i);
+        } else {
+            value = Integer.parseInt(myCharacter.toString());
 
-				if (myCharacter.equals('X') || myCharacter.equals('/') ) {
-					score += 10;
+        }
 
-					if (i + 1 < myString.length()) {
-						score += GetCharValue(myString.charAt(i+1));
+        return value;
+    }
 
-						if ((myCharacter.equals('X')) && (i+2<myString.length())) {
-							score += GetCharValue(myString.charAt(i+2));
-						}
-					}
+    public int getFrameValue(String frame) {
 
-				} else if (myCharacter.equals('-')) {
-					score += 0;
-				} else {
-					score += Integer.parseInt(myCharacter.toString());
-				}
+        int value = this.getTryValue(frame.charAt(0));
 
-			}
+        if (frame.charAt(0) != 'X' && frame.charAt(0) != '/' && 
+                frame.length() > 2 && frame.charAt(1) == '/') {  
 
-			}
+            // If starts with number or "-" and then there is spare (having more strikes)
+            value = this.getTryValue(frame.charAt(1)) + this.getTryValue(frame.charAt(2));
 
-		return score;
-	}
+        }else if (frame.charAt(0) != 'X' && frame.charAt(0) != '/' && 
+                frame.length() > 1 && frame.charAt(1) == '/') {  
+
+            // If starts with number or "-" and then there is spare (no having more strikes)
+            value = this.getTryValue(frame.charAt(1));
+            
+        } else if (frame.charAt(0) != 'X' && frame.charAt(0) != '/' &&
+                frame.length() > 1) {
+
+            // If starts with number or "-" and then there is not spare
+            value += this.getTryValue(frame.charAt(1));
+            
+        }else if (frame.charAt(0) == 'X' && frame.length() == 2){
+            
+            // If we have strike and there is one frame more
+            value += this.getTryValue(frame.charAt(1));
+        
+        }else if (frame.charAt(0) == 'X' && frame.length() > 2){
+            
+            // If we have strike and there is at least two frame more
+            int aux1= this.getTryValue(frame.charAt(1));
+            int aux2 = this.getTryValue(frame.charAt(2));
+            value += aux2;
+            
+            if (aux1 == 10 || aux2 != 10 ){
+                value += aux1;
+            }
+        }
+     
+
+        return value;
+    }
+
+    public int getScore(String myString) {
+
+        int score = 0;
+        int index = 0;
+        boolean finishedGame = false;
+        while (currentFrame < 10 && !finishedGame) {
+
+            score += this.getFrameValue(myString.substring(index));
+
+            if (myString.charAt(index) != 'X' && myString.charAt(index) != '/') {
+                index = index + 2;
+
+            } else {
+                index = index + 1;
+            }
+
+            currentFrame++;
+            
+            if (index >= myString.length())
+                finishedGame = true;
+        }
+
+        return score;
+    }
 
 }
--- jcorbacho/Bowling/test/net/corbacho/AbrilBowling/BowlingTest.java
+++ jcorbacho/Bowling/test/net/corbacho/AbrilBowling/BowlingTest.java
@@ -2,117 +2,135 @@ package net.corbacho.AbrilBowling;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingTest {
 
+    Bowling game = null;
+    
+    @Before
+    public void initialize() throws Exception {
+        game = new Bowling();
+    }
+
+    
 	@Test
 	public void testSetXReturn10() {
-		int out = Bowling.GetValue("X");
+		int out = game.getScore("X");
 		assertEquals(10, out);
 	}
 
 	@Test
-	public void testSetScoreReturn0() {
-		int out = Bowling.GetValue("-");
+	public void testSetDashReturn0() {
+		int out = game.getScore("-");
 		assertEquals(0, out);
 	}
 
 	@Test
 	public void testSetSlashReturn10() {
-		int out = Bowling.GetValue("/");
+		int out = game.getScore("/");
 		assertEquals(10, out);
 	}
 
 	@Test
 	public void testSet1Return1() {
-		assertEquals(1, Bowling.GetValue("1"));
+		assertEquals(1, game.getScore("1"));
 	}
 
 	@Test
 	public void testSet2Return2() {
-		assertEquals(2, Bowling.GetValue("2"));
+		assertEquals(2, game.getScore("2"));
 	}
 
 	@Test
-	public void testSetScorePlusNineReturnNine() {
-		assertEquals(9, Bowling.GetValue("-9"));
+	public void testSetDashNineReturnNine() {
+		assertEquals(9, game.getScore("-9"));
 	}
 
 	@Test
-	public void testSetScorePlusFiveReturnFive() {
-		assertEquals(5, Bowling.GetValue("-5"));
+	public void testSetDashFiveReturnFive() {
+		assertEquals(5, game.getScore("-5"));
 	}
 
 	@Test
-	public void testSetScorePlusXReturnTen() {
-		assertEquals(10, Bowling.GetValue("-X"));
+	public void testSetDashDashReturnTen() {
+		assertEquals(0, game.getScore("--"));
 	}
 
 	@Test
-	public void testSetScorePlusSlashReturnTen() {
-		assertEquals(10, Bowling.GetValue("-/"));
+	public void testSetDashSlashReturnTen() {
+		assertEquals(10, game.getScore("-/"));
 	}
 
 	@Test
-	public void testSetXPlusXReturn30() {
-		assertEquals(30, Bowling.GetValue("XX"));
+	public void testSetXXReturn30() {
+		assertEquals(30, game.getScore("XX"));
 	}
 
 	@Test
-	public void testSetXPlusSlashReturn30() {
-		assertEquals(30, Bowling.GetValue("X/"));
-	}
-
-	@Test
-	public void testSetXPlus9Return28() {
-		assertEquals(28, Bowling.GetValue("X9"));
+	public void testSetX9DashReturn28() {
+		assertEquals(28, game.getScore("X9-"));
 	}
 
 	@Test
 	public void testSetXXXReturn60() {
-		assertEquals(60, Bowling.GetValue("XXX"));
+		assertEquals(60, game.getScore("XXX"));
 	}
 
 	@Test
-	public void testSetXSlashXReturn60() {
-		assertEquals(60, Bowling.GetValue("X/X"));
+	public void testSetXFiveSlashXReturn50() {
+		assertEquals(50, game.getScore("X5/X"));
 	}
 
 	@Test
-	public void testSetSlashSlashXReturn50() {
-		assertEquals(50, Bowling.GetValue("//X"));
+	public void testSetDashSlashXReturn30() {
+		assertEquals(30, game.getScore("-/X"));
 	}
 
 	@Test
-	public void testSetSlashXSlashXReturn80() {
-		assertEquals(80, Bowling.GetValue("/X/X"));
+	public void testSetDashSlashFiveSlashXReturn60() {
+		assertEquals(45, game.getScore("-/5/X"));
 	}
 
+    @Test
+    public void testSetXFiveFiveReturn26() {
+        assertEquals(26, game.getScore("X53"));
+    }
+
+    @Test
+    public void testSetThreeSlash10() {
+        assertEquals(10, game.getScore("3/"));
+    }
+
+    
+    @Test
+    public void testSetXThreeSlashReturn30() {
+        assertEquals(30, game.getScore("X3/"));
+    }
+
+    
 	@Test
-	public void testSetScoreXSlashXReturn60() {
-		assertEquals(60, Bowling.GetValue("-X/X"));
+	public void testSetDash94SlashDashDashReturn19() {
+		assertEquals(19, game.getScore("-94/--"));
 	}
 
+    @Test
+    public void testSetLongFrame1() {
+        assertEquals(90, game.getScore("9-9-9-9-9-9-9-9-9-9-"));
+    }
+
+    @Test
+    public void testSetLongFrame2() {
+        assertEquals(150,game.getScore("5/5/5/5/5/5/5/5/5/5/5"));
+    }
+
+	
 	@Test
-	public void testSetScore9SlashScoreReturn19() {
-		assertEquals(19, Bowling.GetValue("-9/-"));
-	}
-
-	@Test
-	public void testSetLongFrame1() {
-		assertEquals(300, Bowling.GetValue("XXXXXXXXXXXX"));
-	}
-
-	/*@Test
-	public void testSetLongFrame2() {
-		assertEquals(90, Bowling.GetValue("9-9-9-9-9-9-9-9-9-9-"));
-	}*/
-
-	/*@Test
 	public void testSetLongFrame3() {
-		assertEquals(150,Bowling.GetValue("5/5/5/5/5/5/5/5/5/5/5"));
-	}*/
+		assertEquals(300, game.getScore("XXXXXXXXXXXX"));
+	}
+
 
 
 }
