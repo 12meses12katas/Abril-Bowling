fc55bf4b7e002317662f5171e00ee94d8eb6a91e
Silvio René <silvio.rene@gmail.com>
2011-04-27 15:47:57 +0200

    get_value inside of score

--- remosu/bowling.py
+++ remosu/bowling.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 
-def get_value(rolls, i):
-    if rolls[i] == 'X':
-        return 10
-    if rolls[i] == '-':
-        return 0
-    if rolls[i] == '/':
-        return 10 - get_value(rolls, i-1)
-    return int(rolls[i])
 
 def score(rolls):
+    def get_value(i):
+        if rolls[i] == 'X':
+            return 10
+        if rolls[i] == '-':
+            return 0
+        if rolls[i] == '/':
+            return 10 - get_value(i-1)
+        return int(rolls[i])
+
     result = 0
     for i, roll in enumerate(rolls):
         if roll == 'X':
-            result += 10 + get_value(rolls, i+1) + get_value(rolls, i+2)
+            result += 10 + get_value(i+1) + get_value(i+2)
             if i == (len(rolls) - 3):
                 break
         elif roll == '/':
-            result += get_value(rolls, i) + get_value(rolls, i+1)
+            result += get_value(i) + get_value(i+1)
             if i == (len(rolls) - 2):
                 break
         else:
-            result += get_value(rolls, i)
+            result += get_value(i)
     return result
 
 if __name__ == '__main__':
     assert score('XXXXXXXXXXXX') == 300
     assert score('9-9-9-9-9-9-9-9-9-9-') == 90
     assert score('5/5/5/5/5/5/5/5/5/5/5') == 150
+    print ':-D'
