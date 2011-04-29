e5424bafda3ced788ede2f628a586a99d2d6fb07
Rober Martín H <rmh.dev@gmail.com>
2011-04-30 00:58:18 +0800

    (python) Calculating and testing the score of first pin

---
+++ rmhdev/python/bowlingtest.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python
+# coding: utf-8
+
+"""
+Unit test for bowling.py
+"""
+
+__author__ = "Rober Martín H"
+__version__ = "0.1"
+__date__ = "2011-04-26"
+__copyright__ = "Copyright (c) 2011 Rober Martín H"
+__license__ = "MIT License"
+
+import bowling
+import unittest
+
+class TestKnownValues(unittest.TestCase):
+    knownValues = (
+        (0  , "--------------------"),
+        (1  , "1-------------------"),
+        (2  , "2-------------------"),
+        (3  , "3-------------------"),
+        (10  , "X-------------------"),
+    );
+
+    def testGetScore(self):
+        """getScore should give known result with known input"""
+        for number, sequence in self.knownValues:
+            result = bowling.getScore(sequence)
+            self.assertEqual(number, result)
+
+if __name__ == "__main__":
+    unittest.main()
+
