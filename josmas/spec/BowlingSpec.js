d71295a09ee7f6f79714918a6dc91fad13dae9d1
Josmas Flores <josmasflores@gmail.com>
2011-05-01 06:00:05 +0800

    Added constructor for Game through triangulation

--- josmas/spec/BowlingSpec.js
+++ josmas/spec/BowlingSpec.js
@@ -1,23 +1,49 @@
 describe("Bowling Kata", function() {
-    it("Should score Zero when no pins are knocked down in the potential 21 rolls", function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
+    
+    it("Should score Zero when no pins are knocked", function() {
       for (var i = 0; i < 20; i++) {
         game.roll(0); 
       }
       expect(game.score()).toBe(0);
     });
+
+    it("should return 20 with 1 in all rols", function() {
+      for (var i = 0; i < 20; i++) {
+        game.roll(1);
+      };
+      expect(game.score()).toEqual(20);
+    });
+    
+    it("should return 40 with 2 in all rolls", function() {
+      for (var i = 0; i < 20; i++) {
+        game.roll(2);
+      };
+      expect(game.score()).toEqual(40);
+    });
 });  
 
-var game = function() {
+var Game = function() {
+  var totalScore = 0;
+
+  var setTotalScore = function(total) {
+    this.totalScore = 0;
+  };
+
   var roll = function(number) {
-    return 0;
+    totalScore += number;
   };
 
   var score = function() {
-    return 0;
+    return totalScore;
   };
 
   return {
+    setScore: setTotalScore,
     roll: roll,
     score: score
   };
-}();
+};
